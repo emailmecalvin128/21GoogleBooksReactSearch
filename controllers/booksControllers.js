7f3873ff13e83f1f6983ec3df739858fc54fbb1c
Calvin Lee <emailmecalvin@gmail.com>
2020-10-26 04:39:21 -0400

    commit

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -7,7 +7,7 @@ module.exports = {
         db.Book
         .find (req, query)
         .sort ({ date: -1})
-        .then (dbModel => res.json (dbMode1))
+        .then (dbModel => res.json (dbMode))
         .catch(err => {
             console.error(err)
             res.status (422).json (err)
